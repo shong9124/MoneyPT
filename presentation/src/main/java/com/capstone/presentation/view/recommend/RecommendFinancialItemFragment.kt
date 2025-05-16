30ea311347216c2281a6caf2ef8c60d07818446b
shong9124 <shifty393@gmail.com>
2025-05-16 16:52:25 +0900

    로그인 확인 완료

--- presentation/src/main/java/com/capstone/presentation/view/recommend/RecommendFinancialItemFragment.kt
+++ presentation/src/main/java/com/capstone/presentation/view/recommend/RecommendFinancialItemFragment.kt
@@ -2,9 +2,11 @@ package com.capstone.presentation.view.recommend
 
 import com.capstone.presentation.base.BaseFragment
 import com.capstone.presentation.databinding.FragmentRecommendFinancialItemBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RecommendFinancialItemFragment : BaseFragment<FragmentRecommendFinancialItemBinding>() {
     override fun initView() {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
     }
 }
