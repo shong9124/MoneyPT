8e2b0f820d17f7e6bbf09806f86f323a990acc7f
shong9124 <shifty393@gmail.com>
2025-05-25 14:59:38 +0900

    timeout 시간 수정

--- data/src/main/java/com/capstone/data/di/ClientModule.kt
+++ data/src/main/java/com/capstone/data/di/ClientModule.kt
@@ -11,6 +11,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -33,6 +34,9 @@ class ClientModule {
     ): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS) // 연결 타임아웃
+            .readTimeout(30, TimeUnit.SECONDS)    // 응답 대기 타임아웃
+            .writeTimeout(30, TimeUnit.SECONDS)   // 요청 쓰기 타임아웃
             .build()
     }
 
